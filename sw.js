6ec66d125a1a05da1b60ba00c5ced8c72397102a
ruhulamin75 <133599264+ruhulamin75@users.noreply.github.com>
2026-08-29 01:51:58 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'dif-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/1000123161.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
